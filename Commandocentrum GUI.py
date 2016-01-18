5c49594e9f422263bd6b616567f52e40eb6ed12c
Martijn <martijn.dull@student.hu.nl>
2016-01-18 14:18:31 +0100

    Grafische User Interface voor Commandocentrum 1

---
+++ Commandocentrum GUI.py
@@ -0,0 +1,10 @@
+#Deze code wordt gebruikt voor de grafische user interface voor de simulatie van het eerste commandocentrum
+
+from tkinter import *
+
+root = Tk()
+root.wm_title("Commandocentrum 1")
+root.resizable(width=FALSE, height=FALSE)
+root.minsize(width = 1400, height=800)
+
+root.mainloop()
