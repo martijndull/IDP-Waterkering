47f5b756aafcf0a4dccf6729c9c9b8dce2ee36f3
Martijn <martijn.dull@student.hu.nl>
2016-01-21 15:43:55 +0100

    automatische update weer

--- Commandocentrum GUI.py
+++ Commandocentrum GUI.py
@@ -1,19 +1,4 @@
-#Deze code wordt gebruikt voor de grafische user interface voor de simulatie van het eerste commandocentrum
-#toevoegen: weersomstandigheden via API, status / waarschuwing open/sluiten waterkering
-
-
-"""
-locatie
-tijd
-waterniveau
-verwacht waterniveau
-temperatuur
-huidige omstandigheden (bewolking etc)
-luchtvochtigheid
-luchtdruk
-windkracht
-windrichting"""
-
+"""Deze code wordt gebruikt voor de grafische user interface voor de simulatie van het eerste commandocentrum. Op het scherm worden de huidige weersomstandigheden, de huidige waterstand, de status van de Maeslantkering en een klok met de huidige tijd weergegeven."""
 
 from tkinter import *
 import sys
@@ -30,30 +15,36 @@ def waterstanden():
 
     for i in r.json()['features']:
         if r.json()['features'][index_waterstand]['locatienaam'] == 'Hoek van Holland':
-            waterhoogte_hvh = r.json()['features'][index_waterstand]['waarde'] + str("cm boven NAP")
+            waterhoogte_hvh = r.json()['features'][index_waterstand]['waarde']
 
         if r.json()['features'][index_waterstand]['locatienaam'] == 'Rotterdam':
-            waterhoogte_r = r.json()['features'][index_waterstand]['waarde']  + str("cm boven NAP")
+            waterhoogte_r = r.json()['features'][index_waterstand]['waarde']
 
         if r.json()['features'][index_waterstand]['locatienaam'] == 'Dordrecht':
-            waterhoogte_d = r.json()['features'][index_waterstand]['waarde']  + str("cm boven NAP")
+            waterhoogte_d = r.json()['features'][index_waterstand]['waarde']
 
         index_waterstand += 1
 
 waterstanden()
 
-def weersomstandigheden():
-    """Haalt huidige weersomstandigheden van weather.com."""
-    global weather_com_result_hvh, weather_com_result_r, weather_com_result_d
+#kleur waterhoogte
+if int(waterhoogte_hvh) >= 300:
+    kleur_waterhoogte_hvh = 'red'
+else:
+    kleur_waterhoogte_hvh = '#003399'
 
-    weather_com_result_hvh = pywapi.get_weather_from_weather_com('NLXX0025')
-    weather_com_result_r = pywapi.get_weather_from_weather_com('NLXX0015')
-    weather_com_result_d = pywapi.get_weather_from_weather_com('NLXX0006')
+if int(waterhoogte_r) >= 300:
+    kleur_waterhoogte_r = 'red'
+else:
+    kleur_waterhoogte_r = '#003399'
 
-weersomstandigheden()
+if int(waterhoogte_d) >= 300:
+    kleur_waterhoogte_d = 'red'
+else:
+    kleur_waterhoogte_d = '#003399'
 
 #status van kering, input nog aanpassen als rest van code er is.
-status_maeslantkering = input("Maeslantkering: geopend/gesloten?")
+status_maeslantkering = "geopend"
 
 def Commandocentrum_scherm():
     """Maakt GUI voor commandocentrum"""
@@ -74,6 +65,18 @@ def Commandocentrum_scherm():
     waterkering_knop.pack()
     waterkering_knop.place(width=150, height=50, relx=0.45, rely=0.90)
 
+    def weersomstandigheden():
+        """Haalt huidige weersomstandigheden van weather.com."""
+        global weather_com_result_hvh, weather_com_result_r, weather_com_result_d
+
+        weather_com_result_hvh = pywapi.get_weather_from_weather_com('NLXX0025')
+        weather_com_result_r = pywapi.get_weather_from_weather_com('NLXX0015')
+        weather_com_result_d = pywapi.get_weather_from_weather_com('NLXX0006')
+
+        root.after(300000, weersomstandigheden)
+
+
+    weersomstandigheden()
 
     def weer():
         """"Maakt tabel voor weersomstandigheden"""
@@ -92,7 +95,7 @@ def Commandocentrum_scherm():
         #Hoek van Holland
         Label(text="Hoek van Holland", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=0,column=1, sticky=NSEW,)
         Label(text=weather_com_result_hvh['current_conditions']['last_updated'][0:15], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=1,column=1, sticky=NSEW)
-        Label(text=waterhoogte_hvh, anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=1, sticky=NSEW)
+        Label(text=waterhoogte_hvh + str("cm boven NAP"), anchor = NW, bg = 'white', fg=kleur_waterhoogte_hvh, font = ('Ariel',10, 'bold')).grid(row=2,column=1, sticky=NSEW)
         Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=3,column=1, sticky=NSEW)
         Label(text=weather_com_result_hvh['current_conditions']['temperature'] + "°C", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=4,column=1, sticky=NSEW)
         Label(text=weather_com_result_hvh['current_conditions']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=5,column=1, sticky=NSEW,)
@@ -104,7 +107,7 @@ def Commandocentrum_scherm():
         #Rotterdam
         Label(text="Rotterdam", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=0,column=2, sticky=NSEW,)
         Label(text=weather_com_result_r['current_conditions']['last_updated'][0:15], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=1,column=2, sticky=NSEW)
-        Label(text=waterhoogte_r, anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=2, sticky=NSEW)
+        Label(text=waterhoogte_r + str("cm boven NAP"), anchor = NW, bg = 'white', fg=kleur_waterhoogte_r, font = ('Ariel',10, 'bold')).grid(row=2,column=2, sticky=NSEW)
         Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=3,column=2, sticky=NSEW)
         Label(text=weather_com_result_r['current_conditions']['temperature'] + "°C", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=4,column=2, sticky=NSEW)
         Label(text=weather_com_result_r['current_conditions']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=5,column=2, sticky=NSEW,)
@@ -116,7 +119,7 @@ def Commandocentrum_scherm():
         #Dordrecht
         Label(text="Dordrecht", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=0,column=3, sticky=NSEW,)
         Label(text=weather_com_result_d['current_conditions']['last_updated'][0:15], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=1,column=3, sticky=NSEW)
-        Label(text=waterhoogte_d, anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=3, sticky=NSEW)
+        Label(text=waterhoogte_d + str("cm boven NAP"), anchor = NW, bg = 'white', fg=kleur_waterhoogte_d, font = ('Ariel',10, 'bold')).grid(row=2,column=3, sticky=NSEW)
         Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=3,column=3, sticky=NSEW)
         Label(text=weather_com_result_d['current_conditions']['temperature'] + "°C", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=4,column=3, sticky=NSEW)
         Label(text=weather_com_result_d['current_conditions']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=5,column=3, sticky=NSEW,)
@@ -125,16 +128,25 @@ def Commandocentrum_scherm():
         Label(text=weather_com_result_d['current_conditions']['wind']['speed'] + str("km/h"), anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=8,column=3, sticky=NSEW)
         Label(text=weather_com_result_d['current_conditions']['wind']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=9,column=3, sticky=NSEW)
 
+        root.after(300000, weer)
+
+    def status_waarschuwing():
+        """Status van de Maeslantkering en eventuele waarschuwingen"""
         Label(text="Status:", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10,'bold')).grid(row=0, column=6, sticky=NSEW)
         Label(text="De Maeslantkering is op dit moment " + status_maeslantkering + ".", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10,'bold')).grid(row=1, column=6, sticky=NSEW)
+
+        Label(text="Waarschuwingen:", anchor = NW, bg = 'white', fg='red', font = ('Ariel',10,'bold')).grid(row=5, column=6, sticky=NSEW)
+        Label(text="waarschuwing invoegen", anchor = NW, bg = 'white', fg='red', font = ('Ariel',10,'bold')).grid(row=6, column=6, sticky=NSEW)
+
+        root.after(300000, status_waarschuwing)
 
     def update_clock():
         now = time.strftime("%H:%M:%S")
         Label(text=now, bg= 'white').grid(row=0, column=7)
         root.after(1000, update_clock)
 
-
     weer()
+    status_waarschuwing()
     update_clock()
     mainloop()
 
