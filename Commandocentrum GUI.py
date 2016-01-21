85c00d84f62c9e1308e206f3c0757c3b78b5086b
Martijn <martijn.dull@student.hu.nl>
2016-01-21 13:37:57 +0100

    toevoeging waterstanden, omschrijving functies

--- Commandocentrum GUI.py
+++ Commandocentrum GUI.py
@@ -18,18 +18,48 @@ windrichting"""
 from tkinter import *
 import sys
 import pywapi
+import requests
+import json
 
-weather_com_result_hvh = pywapi.get_weather_from_weather_com('NLXX0025')
-weather_com_result_r = pywapi.get_weather_from_weather_com('NLXX0015')
-weather_com_result_d = pywapi.get_weather_from_weather_com('NLXX0006')
+def waterstanden():
+    """Haalt huidige waterstand op van rijkswaterstaat site"""
+    r = requests.get('http://www.rijkswaterstaat.nl/apps/geoservices/rwsnl/?mode=features&projecttype=waterstanden&loadprojects=0')
+    index_waterstand = 0
+    global waterhoogte_hvh, waterhoogte_r, waterhoogte_d
+
+    for i in r.json()['features']:
+        if r.json()['features'][index_waterstand]['locatienaam'] == 'Hoek van Holland':
+            waterhoogte_hvh = r.json()['features'][index_waterstand]['waarde'] + str("cm boven NAP")
+
+        if r.json()['features'][index_waterstand]['locatienaam'] == 'Rotterdam':
+            waterhoogte_r = r.json()['features'][index_waterstand]['waarde']  + str("cm boven NAP")
+
+        if r.json()['features'][index_waterstand]['locatienaam'] == 'Dordrecht':
+            waterhoogte_d = r.json()['features'][index_waterstand]['waarde']  + str("cm boven NAP")
+
+        index_waterstand += 1
+
+waterstanden()
+
+def weersomstandigheden():
+    """Haalt huidige weersomstandigheden van weather.com."""
+    global weather_com_result_hvh, weather_com_result_r, weather_com_result_d
+
+    weather_com_result_hvh = pywapi.get_weather_from_weather_com('NLXX0025')
+    weather_com_result_r = pywapi.get_weather_from_weather_com('NLXX0015')
+    weather_com_result_d = pywapi.get_weather_from_weather_com('NLXX0006')
+
+weersomstandigheden()
 
 #status van kering, input nog aanpassen als rest van code er is
-status_maeslantkering = "geopend."
+status_maeslantkering = input("Maeslantkering: geopend/gesloten?")
 
 def Commandocentrum_scherm():
+    """Maakt GUI voor commandocentrum"""
     root = Tk()
     root.title("Commandocentrum Alpha")
     root.wm_state('zoomed')
+    root.configure(background='white')
 
     weersvoorspelling_knop = Button(text='Weersvoorspelling', bg='#003399', fg='white')
     weersvoorspelling_knop.pack()
@@ -45,6 +75,7 @@ def Commandocentrum_scherm():
 
 
     def weer():
+        """"Maakt tabel voor weersomstandigheden"""
 
         Label(text='Locatie', anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=0,column=0, sticky=NSEW,)
         Label(text='Tijd update', anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=1,column=0, sticky=NSEW,)
@@ -60,7 +91,7 @@ def Commandocentrum_scherm():
         #Hoek van Holland
         Label(text="Hoek van Holland", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=0,column=1, sticky=NSEW,)
         Label(text=weather_com_result_hvh['current_conditions']['last_updated'][0:15], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=1,column=1, sticky=NSEW)
-        Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=1, sticky=NSEW)
+        Label(text=waterhoogte_hvh, anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=1, sticky=NSEW)
         Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=3,column=1, sticky=NSEW)
         Label(text=weather_com_result_hvh['current_conditions']['temperature'] + "°C", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=4,column=1, sticky=NSEW)
         Label(text=weather_com_result_hvh['current_conditions']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=5,column=1, sticky=NSEW,)
@@ -72,7 +103,7 @@ def Commandocentrum_scherm():
         #Rotterdam
         Label(text="Rotterdam", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=0,column=2, sticky=NSEW,)
         Label(text=weather_com_result_r['current_conditions']['last_updated'][0:15], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=1,column=2, sticky=NSEW)
-        Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=2, sticky=NSEW)
+        Label(text=waterhoogte_r, anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=2, sticky=NSEW)
         Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=3,column=2, sticky=NSEW)
         Label(text=weather_com_result_r['current_conditions']['temperature'] + "°C", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=4,column=2, sticky=NSEW)
         Label(text=weather_com_result_r['current_conditions']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=5,column=2, sticky=NSEW,)
@@ -84,7 +115,7 @@ def Commandocentrum_scherm():
         #Dordrecht
         Label(text="Dordrecht", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=0,column=3, sticky=NSEW,)
         Label(text=weather_com_result_d['current_conditions']['last_updated'][0:15], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=1,column=3, sticky=NSEW)
-        Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=3, sticky=NSEW)
+        Label(text=waterhoogte_d, anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=2,column=3, sticky=NSEW)
         Label(text="placeholder", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=3,column=3, sticky=NSEW)
         Label(text=weather_com_result_d['current_conditions']['temperature'] + "°C", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=4,column=3, sticky=NSEW)
         Label(text=weather_com_result_d['current_conditions']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=5,column=3, sticky=NSEW,)
@@ -94,7 +125,7 @@ def Commandocentrum_scherm():
         Label(text=weather_com_result_d['current_conditions']['wind']['text'], anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10, 'bold')).grid(row=9,column=3, sticky=NSEW)
 
         Label(text="Status:", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10,'bold')).grid(row=0, column=6, sticky=NSEW)
-        Label(text="De Maeslantkering is op dit moment " + status_maeslantkering, anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10,'bold')).grid(row=1, column=6, sticky=NSEW)
+        Label(text="De Maeslantkering is op dit moment " + status_maeslantkering + ".", anchor = NW, bg = 'white', fg='#003399', font = ('Ariel',10,'bold')).grid(row=1, column=6, sticky=NSEW)
 
 
     weer()
--- test.py
+++ test.py
@@ -1,3 +1,22 @@
 import requests, json
-r = requests.get('http://www.rijkswaterstaat.nl/apps/geoservices/rwsnl/?mode=features&projecttype=waterstanden&loadprojects=0')
-print (r.json())
+
+def waterstanden():
+    r = requests.get('http://www.rijkswaterstaat.nl/apps/geoservices/rwsnl/?mode=features&projecttype=waterstanden&loadprojects=0')
+    index = 0
+    global waterhoogte_hvh, waterhoogte_r, waterhoogte_d
+
+    for i in r.json()['features']:
+        if r.json()['features'][index]['locatienaam'] == 'Hoek van Holland':
+            waterhoogte_hvh = r.json()['features'][index]['waarde'] + str("cm boven NAP")
+
+        if r.json()['features'][index]['locatienaam'] == 'Rotterdam':
+            waterhoogte_r = r.json()['features'][index]['waarde']  + str("cm boven NAP")
+
+        if r.json()['features'][index]['locatienaam'] == 'Dordrecht':
+            waterhoogte_d = r.json()['features'][index]['waarde']  + str("cm boven NAP")
+
+        index += 1
+
+waterstanden()
+
+print (waterhoogte_hvh)
