199043e69020391832e009d8eadbfc1b800f9681
Martijn <martijn.dull@student.hu.nl>
2016-01-26 13:11:20 +0100

    login eigen python file

--- Commandocentrum GUI.py
+++ Commandocentrum GUI.py
@@ -8,53 +8,10 @@ import json
 import time
 import tkinter.messagebox as tm
 from weersvoorspelling import weersvoorspelling_scherm
+from login import login
 
 #status van kering, input nog aanpassen als rest van code er is.
 status_maeslantkering = "geopend"
-
-def login():
-
-    root_l = Tk()
-    root_l.title("Login")
-    root_l.attributes("-topmost", 1)
-    root_l.attributes("-toolwindow",1)
-    root_l.resizable(0,0)
-    root_l.focus_force()
-    lf = LabelFrame(root_l)
-
-    def _login_btn_clickked():
-        #print("Clicked")
-        username = entry_1.get()
-        password = entry_2.get()
-
-        #print(username, password)
-
-        if username == "admin" and password == "admin":
-            result = tm.askquestion("Afsluiten", "Weet u zeker dat u het programma af wilt sluiten?", icon='warning')
-            if result == 'yes':
-                sys.exit()
-            elif result == 'no':
-                root_l.destroy()
-
-        else:
-            tm.showerror("Fout", "Verkeerde gebruikersnaam of wachtwoord")
-
-    label_1 = Label(lf, text="Gebruikersnaam")
-    label_2 = Label(lf, text="Wachtwoord")
-
-    entry_1 = Entry(lf)
-    entry_2 = Entry(lf, show="*")
-
-    label_1.grid(row=0, sticky=E)
-    label_2.grid(row=1, sticky=E)
-    entry_1.grid(row=0, column=1)
-    entry_2.grid(row=1, column=1)
-
-    logbtn = Button(lf, text="Login", command = _login_btn_clickked)
-    logbtn.grid(columnspan=2)
-
-    lf.pack()
-    root_l.mainloop()
 
 def Commandocentrum_scherm():
     """Maakt GUI voor commandocentrum"""
